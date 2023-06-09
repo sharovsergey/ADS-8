fa84c5b65e5849a110ad499801c0be99113be6e2
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-09 23:47:13 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,49 +1,43 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-Train::Train() {
-    countOp = 0;
-    first = nullptr;
-}
+
 void Train::addCage(bool light) {
-    Cage* nCage = nullptr;
-    if (first == nullptr) {
-        first = new Cage;
-        first->next = first;
-        first->prev = first;
-        first->light = light;
-    } else {
-        nCage = new Cage;
-        nCage->next = first;
-        nCage->prev = first->prev;
-        first->prev = nCage;
-        nCage->prev->next = nCage;
-        nCage->light = light;
-    }
+  if (!first) {
+    Cage* item = new Cage;
+    item->light = light;
+    first = item;
+    last = item;
+  } else {
+    Cage* item = new Cage;
+    item->light = light;
+    last->next = item;
+    item->prev = last;
+    last = item;
+  }
+  last->next = first;
+  first->prev = last;
 }
+
 int Train::getLength() {
-    Cage* lCage = first;
-    lCage->light = true;
-    int count = 1;
-    while (true) {
-        lCage = lCage->next;
-        if (lCage->light == false) {
-            count++;
-            countOp++;
-        } else {
-            lCage->light = false;
-            for (int i = count; i > 0; i--) {
-                lCage = lCage->prev;
-                countOp++;
-            }
-            countOp++;
-            if (lCage->light == false) {
-                return count;
-            } else {
-                count = 1;
-            }
-        }
+  Cage* temp = first;
+  temp->light = true;
+  while (true) {
+    temp = temp->next;
+    count1++;
+    if (temp->light) {
+      temp->light = false;
+      for (int i = count1; i > 0; i--) {
+        temp = temp->prev;
+      }
+      countOp += count1 * 2;
+      if (!temp->light) {
+        return count1;
+      }
+      count1 = 0;
     }
+  }
+  return 0;
 }
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
