2553fbc1e26815962bc68a4e4dfcefedbae17c9a
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-09 22:38:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 // Copyright 2022 NNTU-CS
-#include <iostream>
-#include "train.h"
+#include 
+
+using namespace std;
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCage(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  int n, count = 1;
+  bool light;
+  cout << "Введите количество вагонов: ";
+  cin >> n;
+  cout << "Включить свет в первом вагоне? (1 - да, 0 - нет)\n";
+  cin >> light;
+  bool prevLight = light;
+  for (int i = 2; i <= n; i++) {
+    cout << "Включить свет в " << i << " вагоне? (1 - да, 0 - нет)\n";
+    cin >> light;
+    if (prevLight != light) {
+      count++;
+      prevLight = light;
+    }
+  }
+  if (prevLight != light) {
+    count++;
+  }
+  cout << "Общее количество вагонов: " << count << endl;
   return 0;
 }
